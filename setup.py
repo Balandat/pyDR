b8dad9dc38d7e754cee4abd3cb5587ea667cc229
Maximilian Balandat <balandat@eecs.berkeley.edu>
2016-08-23 09:39:47 -0400

    fixed issue with inclusion of data folder

--- setup.py
+++ setup.py
@@ -10,12 +10,17 @@ reqs = ['numpy>=1.7.1', 'scipy>=0.12', 'pandas>=0.18', 'pytz', 'patsy>=0.4.1']
 # we need logutils for python<3.2
 reqs += ['logutils>=0.3.3']
 
+# add data files to package
+data_dir = os.path.join('pyDR', 'data')
+data_files = [(root, [os.path.join(root, f) for f in files])
+              for root, dirs, files in os.walk(data_dir)]
+
 setup(
     name='pyDR',
     version='0.1',
     packages=['pyDR'],
     install_requires=reqs,
-    package_data={'': ['*.csv']},
+    data_files=data_files,
     author='Maximilian Balandat',
     author_email='balandat@eecss.berkeley.edu',
     description='A package for simulating behavior of consumers ' +
