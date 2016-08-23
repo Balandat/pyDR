b9fe57331182e1a48a73dc90d0df2b53df338335
Maximilian Balandat <balandat@eecs.berkeley.edu>
2016-08-23 10:12:57 -0400

    fixed package data

--- setup.py
+++ setup.py
@@ -10,17 +10,12 @@ reqs = ['numpy>=1.7.1', 'scipy>=0.12', 'pandas>=0.18', 'pytz', 'patsy>=0.4.1']
 # we need logutils for python<3.2
 reqs += ['logutils>=0.3.3']
 
-# add data files to package
-data_dir = os.path.join('pyDR', 'data')
-data_files = [(root, [os.path.join(root, f) for f in files])
-              for root, dirs, files in os.walk(data_dir)]
-
 setup(
     name='pyDR',
     version='0.1.1',
     packages=['pyDR'],
     install_requires=reqs,
-    data_files=data_files,
+    package_data={'pyDR': ['data/*.csv']},
     author='Maximilian Balandat',
     author_email='balandat@eecss.berkeley.edu',
     description='A package for simulating behavior of consumers ' +
